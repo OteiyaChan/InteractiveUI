4da49bbf1f3a77b8a0b2468be8f465e799199d74
Oteiya Chan <oteiyachan@gmail.com>
2025-07-23 15:14:29 -0700

    Whole process

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -8,22 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "What is your name?"
+    
     var body: some View {
         VStack {
-            Text("What is your name?")
+            Text(textTitle)
                 .font(.title)
-            TextField("Type your name here", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+            
+            TextField("Type your name here", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
                 .border(Color.gray, width: 1)
                 .padding()
             Button("Submit name") {
-            }//closes button
+                textTitle = "Welcome, \(name)!"
+                print(name)}//closes button
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.purple)
-            
-            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+
         }
     }
     }
